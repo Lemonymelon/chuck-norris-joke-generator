8fdf40572f080ff9766ef4d22777bf2c231b92fc
Mike Lawlor <shep34@msn.com>
2020-06-23 22:45:07 +0100

    add includes util function, add novel joke logic

--- src/components/Random-joke.tsx
+++ src/components/Random-joke.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { getRandomJokes } from "../api";
+import { includes } from "../utils/es6Methods";
 
 const RandomJoke = () => {
   const [currentJoke, setCurrentJoke] = useState("");
   const [jokeIDHistory, setJokeIDHistory] = useState<number[]>([]);
+  console.log(jokeIDHistory);
 
   interface jokeObject {
     id: number;
@@ -12,9 +14,15 @@ const RandomJoke = () => {
   }
   const handleGetSingleJoke = () => {
     getRandomJokes().then(([{ id, joke }]: jokeObject[]) => {
-      if (id && joke) {
-        setJokeIDHistory([...jokeIDHistory, id]);
-        setCurrentJoke(joke);
+      let isNovel = false;
+      while (!isNovel) {
+        if (id && joke) {
+          if (!includes(id, jokeIDHistory)) {
+            isNovel = true;
+            setJokeIDHistory([...jokeIDHistory, id]);
+            setCurrentJoke(joke);
+          }
+        }
       }
     });
   };
